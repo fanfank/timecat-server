eb5c3e22d3de15f21450508920ed74957ccd7633
fanfank <reetsee@qq.com>
2016-10-16 18:15:51 +0800

    Finish timecat server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# timecat-server
-A server holding demos for https://github.com/fanfank/timecat
+# timecat-server    
+A server holding demos for https://github.com/fanfank/timecat    
+Website：<a href="http://aap.reetsee.com/page/timecat" target="_blank">http://aap.reetsee.com/page/timecat</a>    
--- main.go
+++ main.go
@@ -5,10 +5,11 @@
 package main
 
 import (
+    "bytes"
     "io/ioutil"
     "log"
-    _ "net/http"
     "os"
+    "os/exec"
     "strconv"
     "time"
 
@@ -16,17 +17,33 @@ import (
 )
 
 func main() {
+    _, err := exec.LookPath("timecat")
+    if err != nil {
+        log.Fatalln("`timecat` not found in $PATH")
+    }
+
     router := gin.Default()
-    router.POST("/timecat/api/test", handle)
+    router.POST("/timecat/v1/api/test", handle)
     router.Run(":3193")
 }
 
 func handle(c *gin.Context) {
     logContent := c.PostForm("logContent")
+    st := c.PostForm("st")
+    ed := c.PostForm("ed")
+
     if len([]rune(logContent)) > 1024 * 100 / 2 {
         c.JSON(413, gin.H{
             "errno": -1,
-            "errmsg": "logContent too long",
+            "errmsg": "`logContent` too long",
+        })
+        return
+    }
+
+    if len([]rune(logContent)) == 0 || len([]rune(st)) == 0 || len([]rune(ed)) == 0 {
+        c.JSON(200, gin.H{
+            "errno": -1,
+            "errmsg": "`logContent`, `st`, `ed` can not be empty",
         })
         return
     }
@@ -50,10 +67,31 @@ func handle(c *gin.Context) {
         }
     }()
 
-    //TODO(xuruiqi)
-    c.JSON(200, gin.H{
-        "errno": 0,
-        "errmsg": "success",
-        "data": logContent,
-    })
+    stdout, stderr, err := runTimecat(tmpFilePath, st, ed)
+
+    if err != nil {
+        c.JSON(200, gin.H{
+            "errno": -100,
+            "errmsg": "Ooooops，看来提交的信息不太对劲或`timecat`有Bug，可以直接反馈到`timecat` Issue",
+            "data": err.Error() + ": " + stderr,
+        })
+    } else {
+        c.JSON(200, gin.H{
+            "errno": 0,
+            "errmsg": "success",
+            "data": stdout,
+        })
+    }
+}
+
+func runTimecat(tmpFilePath, st, ed string) (string, string, error) {
+    var stdout bytes.Buffer
+    var stderr bytes.Buffer
+
+    cmd := exec.Command("timecat", "-s", st, "-e", ed, tmpFilePath)
+    cmd.Stdout = &stdout
+    cmd.Stderr = &stderr
+
+    err := cmd.Run()
+    return stdout.String(), stderr.String(), err
 }
